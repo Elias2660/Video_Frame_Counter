468957bf9372b31974e64f6ca0367d48e0328541
Elias Xu <124101026+Elias2660@users.noreply.github.com>
2024-07-08 21:03:11 -0400

    rework logging

--- converter.py
+++ converter.py
@@ -4,7 +4,6 @@ import numpy as np
 import os
 import logging
 import threading
-import sys
 import argparse
 import cv2
 import concurrent.futures
@@ -13,12 +12,12 @@ import subprocess
 
 format = "%(asctime)s: %(message)s"
 logging.basicConfig(format=format, level=logging.INFO, datefmt="%H:%M:%S")
-logging.getLogger().setLevel(logging.DEBUG)
+# logging.getLogger().setLevel(logging.DEBUG)
 
 parser = argparse.ArgumentParser(description="Create counts.csv file")
 
 parser.add_argument("--path", type=str, help="Path to the directory containing the video files", required=True)
-
+parser.add_argument("--max-workers", type=int, help="Number of threads to use", default=20)
 args = parser.parse_args()
 original_path = os.path.join(os.getcwd(), args.path)
 
@@ -37,13 +36,13 @@ def count_frames(original_path: str, file:str) -> int:
         count = 0
         while cap.isOpened():
             ret, _ = cap.read()
-            if count % 300 == 0:
-                logging.debug(f"Frame {count} read from {file}")
+            if count % 1500 == 0:
+                logging.info(f"Frame {count} read from {file}")
             if not ret:
                 break
             count += 1
         with _lock:
-            logging.debug(f"Adding {file} to DataFrame")
+            logging.info(f"Adding {file} to DataFrame")
             new_row = pd.DataFrame([[file, count]], columns=["filename", "framecount"])
             dataframe = pd.concat([dataframe, new_row], ignore_index=True)
         cap.release()
@@ -57,16 +56,18 @@ command = "ls | grep -E '.h264|.mp4'"
 ansi_escape = re.compile(r'\x1B\[[0-?]*[ -/]*[@-~]')
 result = subprocess.run(command, shell=True, capture_output=True, text=True)
 file_list = sorted([ansi_escape.sub('', line) for line in result.stdout.splitlines()])
-
+logging.debug(f"File List: {file_list}")
 
 threads = list()
-print(f"File List: {file_list}")
+logging.info(f"File List: {file_list}")
 
-with concurrent.futures.ThreadPoolExecutor(max_workers=20) as executor:
+with concurrent.futures.ThreadPoolExecutor(max_workers=args.max_workers) as executor:
+    logging.debug(f"Executor established")
     executor.map(count_frames, [original_path]*len(file_list), file_list)
     
+logging.debug(f"DataFrame about to be sorted")
 dataframe = dataframe.sort_values(by="filename")
-
+logging.debug(f"DataFrame about to be saved")
 dataframe.to_csv(os.path.join(original_path, "counts.csv"), index=False)
 
 
